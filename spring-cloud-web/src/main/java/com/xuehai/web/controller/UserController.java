658a092039d0995c317282c0563aa0c326f4c858
Administrator <394500424@qq.com>
2019-02-01 15:46:36 +0800

    spring cloud demo

--- spring-cloud-web/src/main/java/com/xuehai/web/controller/UserController.java
+++ spring-cloud-web/src/main/java/com/xuehai/web/controller/UserController.java
@@ -2,6 +2,8 @@ package com.xuehai.web.controller;
 
 import com.xuehai.web.entity.UserEntity;
 import com.xuehai.web.service.UserService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value="获取用户详细信息", notes="根据用户id来获取用户信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
     @RequestMapping({"/getUserInfo/{id}"})
     public UserEntity getUserInfo(@PathVariable Integer id){
 
